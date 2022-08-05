066bdf2e11dfee46da483da2aab0c9e14702e1a5
SaduBalaji <97277651+SaduBalaji@users.noreply.github.com>
2022-08-05 19:44:39 +0530

    done

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
   }
   required_version = ">= 1.0.4"
   cloud {
-    organization = "aws-poc-cloud"
+    organization = "terraform-www-aws"
 
     workspaces {
-      name = "aws-terraformcloud-github-poc"
+      name = "terraformWS"
     }
   }
 }
